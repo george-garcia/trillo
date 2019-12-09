44cc80345dbc2480f7440c394862bd42d857baae
BreezyP <jorge.a.garcia12@gmail.com>
2019-12-08 19:28:03 -0600

    styled the sidebar and navigation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../img/logo.png';
 import svg from '../img/sprite.svg';
+import avatar from '../img/user.jpg';
 
 const Header = () => {
 
@@ -17,10 +18,70 @@ const Header = () => {
                         </svg>
                     </button>
                 </form>
+
+                <nav className="user-nav">
+                    <div className="user-nav__icon-box">
+                        <svg className="user-nav__icon">
+                            <use xlinkHref={`${svg}#icon-bookmark`}></use>
+                        </svg>
+                        <span className="user-nav__notification">7</span>
+                    </div>
+                    <div className="user-nav__icon-box">
+                        <svg className="user-nav__icon">
+                            <use xlinkHref={`${svg}#icon-chat`}></use>
+                        </svg>
+                        <span className="user-nav__notification">13</span>
+                    </div>
+
+                    <div className="user-nav__user">
+                        <img src={avatar} alt="" className="user-nav__user-photo"/>
+                        <span className="user-nav__user-name">Jonas</span>
+                    </div>
+                </nav>
             </header>
+
+
             <div className="content">
                 <nav className="sidebar">
-                    Navigation
+                    <ul className="side-nav">
+                        <li className="side-nav__item side-nav__item--active">
+                            <a href="#" className="side-nav__link">
+                                <svg className="side-nav__icon">
+                                    <use xlinkHref={`${svg}#icon-home`}></use>
+                                </svg>
+                                <span>Hotel</span>
+                            </a>
+                        </li>
+                        <li className="side-nav__item">
+                            <a href="#" className="side-nav__link">
+                                <svg className="side-nav__icon">
+                                    <use xlinkHref={`${svg}#icon-aircraft-take-off`}></use>
+                                </svg>
+                                <span>Flight</span>
+                            </a>
+                        </li>
+                        <li className="side-nav__item">
+                            <a href="#" className="side-nav__link">
+                                <svg className="side-nav__icon">
+                                    <use xlinkHref={`${svg}#icon-key`}></use>
+                                </svg>
+                                <span>Car rental</span>
+                            </a>
+                        </li>
+                        <li className="side-nav__item">
+                            <a href="#" className="side-nav__link">
+                                <svg className="side-nav__icon">
+                                    <use xlinkHref={`${svg}#icon-map`}></use>
+                                </svg>
+                                <span>Tours</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className="legal">
+                        &copy; 2019 by trillo. All rights reserved.
+                    </div>
+
                 </nav>
                 <main className="hotel-view">
                     Hotel view
